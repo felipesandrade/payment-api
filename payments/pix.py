ed87cc5a24daadd21ab4ac5525698a54ae59e1ce
Felipe Andrade <felipes.andrade@gmail.com>
2025-04-28 22:09:04 -0300

    Updating unit test

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,8 @@ class Pix():
     def __init__(self):
         pass
 
-    def create_payment(self):
+    # base_dir created to use in unit tests   
+    def create_payment(self, base_dir=""):
         # Create the payment at the bank
         bank_payment_id = str(uuid.uuid4())
 
@@ -16,9 +17,9 @@ class Pix():
         img = qrcode.make(hash_payment)
         
         # Save qrcode image as png
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
         
         return {
-                "payment_bank_id": bank_payment_id,
+                "bank_payment_id": bank_payment_id,
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}" 
                 }
